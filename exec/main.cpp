962235f1d0de515280a95f0e7f190b25577099af
tck199732 <ctb8135@wmich.edu>
2023-10-16 12:26:43 -0400

    ⚗️test main script

--- exec/main.cpp
+++ exec/main.cpp
@@ -13,20 +13,25 @@ int main(int argc, char *argv[])
     custom_pair_cut* prcut = new custom_pair_cut();
     custom_correlation* corr = new custom_correlation(50, 0., 200.);
 
-    evcut->set_impact_parameter(0.0, 3.0);
-    evcut->set_multiplicity(0, 128);
-    trkcut1->set_neutron(1);
-    trkcut1->set_proton(1);
-    trkcut1->set_transverse_momentum(0, 500);
-    trkcut1->set_detector_idx(0);
+    evcut->set_accepted_impact_parameter(0, 1e5);
+    evcut->set_accepted_multiplicity(0, 128);
+    evcut->set_accepted_trigger_condition("");
+    evcut->set_accepted_trigger_value(0, 1e5);
+
+    trkcut1->set_accepted_neutron(1);
+    trkcut1->set_accepted_proton(1);
+    trkcut1->set_transverse_momentum(0, 1e5);
+    trkcut1->set_accepted_detector_idx(0);
+    trkcut1->set_accepted_efficiency(0, 1e5);
     
     // if identical particle, second track cut should be set as first track cut
-    trkcut2->set_neutron(2);
-    trkcut2->set_proton(2);
+    trkcut2->set_accepted_neutron(2);
+    trkcut2->set_accepted_proton(2);
     trkcut2->set_transverse_momentum(0, 1e5);
-    trkcut2->set_detector_idx(0);
+    trkcut2->set_accepted_detector_idx(0);
+    trkcut2->set_accepted_efficiency(0, 1e5);
 
-    prcut->set_transverse_momentum(0, 1000);
+    prcut->set_transverse_momentum(0, 1e5);
 
     anal->set_event_cut(evcut);
     anal->set_first_track_cut(trkcut1);
@@ -45,6 +50,12 @@ int main(int argc, char *argv[])
         reader_status = man->process();
         nevents_processed++;
     }
+
+    std::cout << trkcut1->get_ntracks_passed() << std::endl;
+    std::cout << trkcut1->get_ntracks_failed() << std::endl;
+    std::cout << trkcut2->get_ntracks_passed() << std::endl;
+    std::cout << trkcut2->get_ntracks_failed() << std::endl;
+
 
     TFile *outf = new TFile("output.root", "RECREATE");
     corr->Write();
