e12e82469e3df55ec7ee0fa89d2d9d01f7c38f94
tck199732 <ctb8135@wmich.edu>
2023-10-17 14:59:10 -0400

    :fire: rm detector index

--- custom/readers/custom_track.cpp
+++ custom/readers/custom_track.cpp
@@ -2,7 +2,6 @@
 
 custom_track::custom_track() : track()
 {
-    this->detector_index = INT_MAX;
     this->efficiency = 1.;
 }
 
@@ -10,11 +9,10 @@ custom_track::custom_track(
     const unsigned int &N, const unsigned int &Z,
     const double &vx, const double &vy, const double &vz, const double &mass,
     const double &x, const double &y, const double &z, const double &t,
-    const int &index, const double &eff
+    const double &eff
     //
     ) : track(N, Z, vx, vy, vz, mass, x, y, z, t)
 {
-    this->detector_index = index;
     this->efficiency = eff;
 }
 
@@ -23,15 +21,15 @@ custom_track::custom_track(
     const double &mass, 
     const std::array<double, 3> &v,
     const std::array<double, 4> &x,
-    const int &index, const double &eff
+    const double &eff
     //
     ) : track(N, Z, mass, v, x)
 {
-    this->detector_index = index;
     this->efficiency = eff;
 }
 
 custom_track::custom_track(const custom_track & ctrk) : track(ctrk) {
-    this->detector_index = ctrk.detector_index;
     this->efficiency = ctrk.efficiency;
 }
+
+custom_track::~custom_track() {;}
--- custom/readers/custom_track.hpp
+++ custom/readers/custom_track.hpp
@@ -11,7 +11,7 @@ public:
         const unsigned int &N, const unsigned int &Z,
         const double &vx, const double &vy, const double &vz, const double &mass,
         const double &x = 0., const double &y = 0., const double &z = 0., const double &t = 0.,
-        const int &index = 0., const double &eff = 0.
+        const double &eff = 0.
         //
     );
 
@@ -19,20 +19,16 @@ public:
         const unsigned int &N, const unsigned int &Z,
         const double &mass, const std::array<double, 3> &v,
         const std::array<double, 4> &x = {0., 0., 0., 0.},
-        const int &index = 0., const double &eff = 0.
+        const double &eff = 0.
         //
     );
 
     custom_track(const custom_track &);
-    ~custom_track() {;}
+    ~custom_track();
 
-    void set_detector_index(const int & idx) { this->detector_index = idx; }
     void set_efficiency(const double & eff) { this->efficiency = eff; }
-
-    int get_detector_index() const { return this->detector_index; }
     double get_efficiency() const { return this->efficiency; }
 private:
-    int detector_index;
     double efficiency;
 };
 
