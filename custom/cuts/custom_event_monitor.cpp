197705a8072fa556d0cf4094dd2b42941dea4d1e
tck199732 <ctb8135@wmich.edu>
2023-10-18 14:32:05 -0400

    :sparkles: add sample monitor cls

---
+++ custom/cuts/custom_event_monitor.cpp
@@ -0,0 +1,30 @@
+#include "cuts/custom_event_monitor.hpp"
+
+custom_event_monitor::custom_event_monitor(const std::string &name)
+{
+    this->name = name;
+    this->h1_multiplicity = new TH1D("h1_multiplicity", "", 20, -0.5, 19.5);
+    this->h1_impact_parameter = new TH1D("h1_impact_parameter", "", 100., 0., 1.);
+    this->h1_multiplicity->Sumw2();
+    this->h1_impact_parameter->Sumw2();
+    this->h1_multiplicity->SetDirectory(0);
+    this->h1_impact_parameter->SetDirectory(0);
+}
+
+custom_event_monitor::~custom_event_monitor()
+{
+    delete h1_multiplicity;
+    delete h1_impact_parameter;
+}
+
+void custom_event_monitor::fill(const event *evt)
+{
+    this->h1_multiplicity->Fill(evt->get_multiplicity());
+    this->h1_impact_parameter->Fill(evt->get_impact_parameter());
+}
+
+void custom_event_monitor::write()
+{
+    this->h1_multiplicity->Write();
+    this->h1_impact_parameter->Write();
+}
---
+++ custom/cuts/custom_pair_monitor.hpp
@@ -0,0 +1,24 @@
+#ifndef custom_pair_monitor_hpp
+#define custom_pair_monitor_hpp
+
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TMath.h"
+#include "pair.hpp"
+#include "monitor.hpp"
+
+class custom_pair_monitor : public monitor
+{
+public:
+    custom_pair_monitor(const std::string &name = "");
+    virtual ~custom_pair_monitor();
+    virtual void fill(const pair *evt);
+    virtual void report() { ; }
+    void write();
+
+private:
+    std::string name;
+    TH1D *h1_transverse_velocity;
+};
+
+#endif
