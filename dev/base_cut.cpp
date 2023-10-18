31d770d32b28cc49339f4499850c9d7ff8788da5
tck199732 <ctb8135@wmich.edu>
2023-10-18 14:32:53 -0400

    :pencil2: fill not Fill

--- dev/base_cut.cpp
+++ dev/base_cut.cpp
@@ -23,24 +23,23 @@ base_cut::~base_cut()
 void base_cut::fill_monitor(const event *evt, bool pass)
 {
     if (pass && this->passed_monitor != nullptr)
-        this->passed_monitor->Fill(evt);
+        this->passed_monitor->fill(evt);
     else if (!pass && this->failed_monitor != nullptr)
-        this->failed_monitor->Fill(evt);
+        this->failed_monitor->fill(evt);
 }
 
 void base_cut::fill_monitor(const track *trk, bool pass)
 {
     if (pass && this->passed_monitor != nullptr)
-        this->passed_monitor->Fill(trk);
+        this->passed_monitor->fill(trk);
     else if (!pass && this->failed_monitor != nullptr)
-        this->failed_monitor->Fill(trk);
+        this->failed_monitor->fill(trk);
 }
 
 void base_cut::fill_monitor(const pair *pr, bool pass)
 {
     if (pass && this->passed_monitor != nullptr)
-        this->passed_monitor->Fill(pr);
+        this->passed_monitor->fill(pr);
     else if (!pass && this->failed_monitor != nullptr)
-        this->failed_monitor->Fill(pr);
+        this->failed_monitor->fill(pr);
 }
-
