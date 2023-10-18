761a33ab05de442dab04ad4f68efeab4f34c20d5
tck199732 <ctb8135@wmich.edu>
2023-10-18 14:32:32 -0400

    :sparkles: add getters

--- dev/manager.hpp
+++ dev/manager.hpp
@@ -8,9 +8,13 @@ class manager
 public:
     manager();
     virtual ~manager();
+    int process();
+
     void set_reader(reader * reader) { event_reader = reader; }
     void add_analysis(analysis * anal) { analyses->push_back(anal);}
-    int process();
+
+    reader* get_reader() const { return event_reader; }
+    analysis_collection* get_analyses() const { return analyses; }
 
 private:
     reader *event_reader;
