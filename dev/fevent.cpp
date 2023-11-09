16ab70ad6edab32b52193ecab9ecd54e1d1949d1
tck199732 <ctb8135@wmich.edu>
2023-11-09 13:14:44 -0500

    :art: add braces

--- dev/fevent.cpp
+++ dev/fevent.cpp
@@ -21,10 +21,12 @@ fevent::fevent(const fevent &evt) {
 }
 
 fevent::~fevent() {
-	for (auto &ptcl : *first_collection)
+	for (auto &ptcl : *first_collection) {
 		delete ptcl;
-	for (auto &ptcl : *second_collection)
+	}
+	for (auto &ptcl : *second_collection) {
 		delete ptcl;
+	}
 	first_collection->clear();
 	second_collection->clear();
 	delete first_collection;
