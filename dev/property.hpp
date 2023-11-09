fff7a7d0b02500b5aebd855d2c87447654f10b22
tck199732 <ctb8135@wmich.edu>
2023-11-08 21:36:22 -0500

    🥅assert unique_ptr in std::any

--- dev/property.hpp
+++ dev/property.hpp
@@ -4,8 +4,10 @@
 #include <any>
 #include <iostream>
 #include <map>
+#include <memory>
 #include <stdexcept>
 #include <string>
+#include <type_traits>
 
 class property {
 public:
@@ -19,6 +21,9 @@ public:
 	}
 
 	template <typename T> void set_property(const std::string &key, const T &value) {
+		// avoid setting unique_ptr to property, check at compile time
+		static_assert(!std::is_same_v<T, std::unique_ptr<std::remove_pointer_t<T>>>,
+					  "property::set_property: unique_ptr is not allowed.");
 		this->properties[key] = value;
 	}
 
@@ -30,7 +35,7 @@ public:
 		try {
 			return std::any_cast<T>(this->properties.at(key));
 		} catch (const std::bad_any_cast &e) {
-			std::cerr << "property::get_property: " << e.what() << "of key : " << key << '\n';
+			std::cerr << "property::get_property: " << e.what() << " of key : " << key << '\n';
 			throw std::bad_any_cast();
 		} catch (const std::out_of_range &e) {
 			throw std::out_of_range("property::get_property: " + key + " not found.");
