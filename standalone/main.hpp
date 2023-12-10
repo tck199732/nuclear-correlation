bd89f8ad6ca5298b6cd456ce904ffbe95f8e24ab
tck199732 <ctb8135@wmich.edu>
2023-12-10 00:48:07 -0500

    Add custom and external libraries to main.hpp

--- standalone/main.hpp
+++ standalone/main.hpp
@@ -1,6 +1,8 @@
+// dev library
 #include "analysis.hpp"
 #include "manager.hpp"
 
+// custom library
 #include "correlations/custom_correlation.hpp"
 #include "cuts/custom_event_cut.hpp"
 #include "cuts/custom_event_monitor.hpp"
@@ -9,3 +11,14 @@
 #include "cuts/custom_track_cut.hpp"
 #include "cuts/custom_track_monitor.hpp"
 #include "readers/custom_reader.hpp"
+
+// external library
+#include "argparse/argparse.hpp"
+#include "indicators/block_progress_bar.hpp"
+#include "indicators/cursor_control.hpp"
+#include <indicators/progress_bar.hpp>
+using namespace indicators;
+
+BlockProgressBar pbar{option::BarWidth{80}, option::Start{"["}, option::End{"]"},
+					  option::ForegroundColor{Color::white},
+					  option::FontStyles{std::vector<FontStyle>{FontStyle::bold}}};
