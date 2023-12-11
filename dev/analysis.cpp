b82d9468f184629804fee2fea3f7c0f83c18d045
tck199732 <ctb8135@wmich.edu>
2023-12-11 00:07:53 -0500

    Refactor analysis class and add new methods

--- dev/analysis.cpp
+++ dev/analysis.cpp
@@ -1,85 +1,63 @@
 #include "analysis.hpp"
-analysis::analysis() {
-	event_mixing_size = 5;
-	evt_cut = nullptr;
-	first_track_cut = nullptr;
-	second_track_cut = nullptr;
-	real_pr_cut = nullptr;
-	mixed_pr_cut = nullptr;
-	correlations = new correlation_collection();
-	event_mixing_buffer = new fevent_collection();
+analysis::analysis(const std::string &name, const int &mixing_size) : name(name), event_mixing_size(mixing_size) {
+	this->evt_cut = nullptr;
+	this->first_track_cut = nullptr;
+	this->second_track_cut = nullptr;
+	this->real_pair_cut = nullptr;
+	this->mixed_pair_cut = nullptr;
+	this->correlations = new correlation_collection();
+	this->event_mixing_buffer = new fevent_collection();
 }
 
-analysis::analysis(const analysis &anal) {
-	this->event_mixing_size = anal.event_mixing_size;
-	this->evt_cut = 0;
-	this->first_track_cut = 0;
-	this->second_track_cut = 0;
-	this->real_pr_cut = 0;
-	this->mixed_pr_cut = 0;
-	this->correlations = 0;
-	this->event_mixing_buffer = 0;
+analysis::analysis(const analysis &other) {
+	this->event_mixing_size = other.event_mixing_size;
+	this->evt_cut = other.evt_cut;
+	this->first_track_cut = other.first_track_cut;
+	this->second_track_cut = other.second_track_cut;
+	this->real_pair_cut = other.real_pair_cut;
+	this->mixed_pair_cut = other.mixed_pair_cut;
+	this->correlations = other.correlations;
+	this->event_mixing_buffer = other.event_mixing_buffer;
 }
 
 analysis::~analysis() {
-	if (evt_cut) {
-		delete evt_cut;
-	}
-	if (is_identical_particle()) {
-		second_track_cut = nullptr;
-	}
-	if (first_track_cut) {
-		delete first_track_cut;
-		first_track_cut = nullptr;
-	}
-	if (second_track_cut) {
-		delete second_track_cut;
-		second_track_cut = nullptr;
-	}
-	if (real_pr_cut) {
-		delete real_pr_cut;
-		real_pr_cut = nullptr;
-	}
-	if (mixed_pr_cut) {
-		delete mixed_pr_cut;
-		mixed_pr_cut = nullptr;
-	}
-	if (correlations) {
-		for (auto &corr : *correlations) {
-			delete corr;
-		}
-		delete correlations;
-		correlations = nullptr;
-	}
-	if (event_mixing_buffer) {
-		for (auto &fevt : *event_mixing_buffer) {
-			delete fevt;
-		}
-		delete event_mixing_buffer;
-		event_mixing_buffer = nullptr;
-	}
-}
-
-fevent *analysis::preprocess(const event *evt) {
-	auto fevt = new fevent();
-	auto tracks = evt->get_track_collection();
-	auto first_collection = fevt->get_first_collection();
-	fill_particles(first_track_cut, tracks, first_collection);
-	if (!is_identical_particle()) {
-		auto second_collection = fevt->get_second_collection();
-		fill_particles(second_track_cut, tracks, second_collection);
-	}
-	return fevt;
-}
-
-void analysis::process(const event *evt) {
-	// event will be passed if event_cut == nullptr
 	if (this->evt_cut) {
-		bool is_passed_event = this->evt_cut->pass(evt);
-		this->evt_cut->fill_monitor(evt, is_passed_event);
-		if (!is_passed_event) {
-			return;
-		}
+		delete this->evt_cut;
+	}
+	if (this->first_track_cut) {
+		delete this->first_track_cut;
+	}
+	if (this->second_track_cut) {
+		delete this->second_track_cut;
+	}
+	if (this->real_pair_cut) {
+		delete this->real_pair_cut;
+	}
+	if (this->mixed_pair_cut) {
+		delete this->mixed_pair_cut;
+	}
+
+	for (auto &corr : *(this->correlations)) {
+		delete corr;
+	}
+	this->correlations->clear();
+	delete this->correlations;
+
+	for (auto &fevt : *(this->event_mixing_buffer)) {
+		delete fevt;
+	}
+	this->event_mixing_buffer->clear();
+	delete this->event_mixing_buffer;
+}
+
+void analysis::process(event *&evt) {
+	// event will be passed if event_cut == nullptr
+	bool pass = this->evt_cut ? this->evt_cut->pass(evt) : true;
+	if (this->evt_cut) {
+		this->evt_cut->fill_monitor(evt, pass);
+	}
+	if (!pass) {
+		return;
 	}
 
 	// retain only the passed particles
@@ -120,12 +98,11 @@ void analysis::process(const event *evt) {
 	}
 }
 
-void analysis::fill_real_correlation(particle_collection *first, particle_collection *second) {
-	pair *pr = new pair();
+void analysis::fill_real_correlation(track_collection *&first, track_collection *&second) {
 	auto start_outer = first->begin();
 	auto end_outer = first->end();
-	particle_collection::iterator start_inner;
-	particle_collection::iterator end_inner;
+	track_collection::iterator start_inner;
+	track_collection::iterator end_inner;
 
 	if (!second) {
 		end_inner = first->end();
@@ -142,14 +119,11 @@ void analysis::fill_real_correlation(particle_collection *first, particle_collec
 			start_inner++;
 		}
 		for (auto &jptcl = start_inner; jptcl != end_inner; jptcl++) {
-
-			pr->set_first_particle(*iptcl);
-			pr->set_second_particle(*jptcl);
-
+			auto pr = std::make_pair(*iptcl, *jptcl);
 			bool is_passed_pair = true;
-			if (real_pr_cut) {
-				is_passed_pair = real_pr_cut->pass(pr);
-				real_pr_cut->fill_monitor(pr, is_passed_pair);
+			if (real_pair_cut) {
+				is_passed_pair = real_pair_cut->pass(pr);
+				real_pair_cut->fill_monitor(pr, is_passed_pair);
 			}
 			if (is_passed_pair) {
 				for (auto &corr : *correlations) {
@@ -158,48 +132,56 @@ void analysis::fill_real_correlation(particle_collection *first, particle_collec
 			}
 		}
 	}
-	delete pr;
 }
 
-void analysis::fill_mixed_correlation(particle_collection *first, particle_collection *second) {
-	pair *pr = new pair();
+void analysis::fill_mixed_correlation(track_collection *&first, track_collection *&second) {
+
 	for (auto &iptcl : *first) {
 		for (auto &jptcl : *second) {
-			pr->set_first_particle(iptcl);
-			pr->set_second_particle(jptcl);
+			auto pr = std::make_pair(iptcl, jptcl);
+			bool pass = this->mixed_pair_cut ? this->mixed_pair_cut->pass(pr) : true;
 
-			bool is_passed_pair = true;
-			if (mixed_pr_cut) {
-				is_passed_pair = mixed_pr_cut->pass(pr);
-				mixed_pr_cut->fill_monitor(pr, is_passed_pair);
+			if (this->mixed_pair_cut) {
+				this->mixed_pair_cut->fill_monitor(pr, pass);
 			}
-			if (is_passed_pair) {
-				for (auto &corr : *correlations) {
-					corr->add_mixed_pair(pr);
-				}
+			if (!pass) {
+				continue;
+			}
+			for (auto &corr : *(this->correlations)) {
+				corr->add_mixed_pair(pr);
 			}
 		}
 	}
-	delete pr;
 }
 
-void analysis::fill_particles(track_cut *cut, track_collection *src, particle_collection *des) {
-	for (auto &trk : *src) {
+fevent *analysis::preprocess(const event *evt) {
+	auto fevt = new fevent();
+	auto tracks = evt->get_track_collection();
+	auto first_collection = fevt->get_first_collection();
+	fill_particles(first_track_cut, tracks, first_collection);
+	if (!is_identical_particle()) {
+		auto second_collection = fevt->get_second_collection();
+		fill_particles(second_track_cut, tracks, second_collection);
+	}
+	return fevt;
+}
+
+void analysis::fill_particles(track_cut *&cut, track_collection *&src, track_collection *&des) {
+	// pre-clean the destination collection
+	for (auto &des_track : *des) {
+		if (des_track) {
+			delete des_track;
+		}
+	}
+	// fill particles into the destination collection and monitors
+	for (auto &src_track : *src) {
+		bool pass = cut != nullptr ? cut->pass(src_track) : true;
 		if (cut) {
-			bool is_passed_track = cut->pass(trk);
-			cut->fill_monitor(trk, is_passed_track);
-			if (is_passed_track)
-				des->push_back(new particle(trk));
-		} else {
-			des->push_back(new particle(trk));
+			cut->fill_monitor(src_track, pass);
+		}
+		if (pass) {
+			des->push_back(new track(*src_track));
 		}
 	}
 	return;
-}
-
-correlation *analysis::get_correlation(const unsigned int &index) const {
-	if (index >= correlations->size()) {
-		return nullptr;
-	}
-	return (*correlations)[index];
 }
--- dev/analysis.hpp
+++ dev/analysis.hpp
@@ -12,54 +12,68 @@
 #include "event.hpp"
 #include "event_cut.hpp"
 #include "fevent.hpp"
-#include "pair.hpp"
 #include "pair_cut.hpp"
-#include "particle.hpp"
 #include "reader.hpp"
 #include "track.hpp"
 #include "track_cut.hpp"
 
 class analysis {
 public:
-	analysis();
+	analysis(const std::string &name = "", const int &mixing_size = 5);
 	analysis(const analysis &);
 	virtual ~analysis();
+	virtual void process(event *&evt);
 
-	fevent *preprocess(const event *evt);
-	virtual void process(const event *evt);
-	void fill_real_correlation(particle_collection *first, particle_collection *second = nullptr);
-	void fill_mixed_correlation(particle_collection *first, particle_collection *second);
-
-	void set_event_mixing_size(const unsigned int &size) { event_mixing_size = size; }
-	void set_event_cut(event_cut *cut) { evt_cut = cut; }
-	void set_first_track_cut(track_cut *cut) { first_track_cut = cut; }
-	void set_second_track_cut(track_cut *cut) { second_track_cut = cut; }
-	void set_real_pair_cut(pair_cut *cut) { real_pr_cut = cut; }
-	void set_mixed_pair_cut(pair_cut *cut) { mixed_pr_cut = cut; }
-	void add_correlation(correlation *corr) { correlations->push_back(corr); }
+	void set_event_mixing_size(const unsigned int &size);
+	void set_event_cut(event_cut *cut);
+	void set_first_track_cut(track_cut *cut);
+	void set_second_track_cut(track_cut *cut);
+	void set_real_pair_cut(pair_cut *cut);
+	void set_mixed_pair_cut(pair_cut *cut);
+	void add_correlation(correlation *corr);
 
 	unsigned int get_event_mixing_size() const { return event_mixing_size; }
 	event_cut *get_event_cut() const { return evt_cut; }
 	track_cut *get_first_track_cut() const { return first_track_cut; }
 	track_cut *get_second_track_cut() const { return second_track_cut; }
-	pair_cut *get_real_pair_cut() const { return real_pr_cut; }
-	pair_cut *get_mixed_pair_cut() const { return mixed_pr_cut; }
-	correlation *get_correlation(const unsigned int &index) const;
+	pair_cut *get_real_pair_cut() const { return real_pair_cut; }
+	pair_cut *get_mixed_pair_cut() const { return mixed_pair_cut; }
+	correlation *get_correlation(const unsigned int &index) const { return correlations->at(index); }
 
 protected:
+	std::string name;
 	unsigned int event_mixing_size;
+
 	event_cut *evt_cut;
 	track_cut *first_track_cut;
 	track_cut *second_track_cut;
-	pair_cut *real_pr_cut;
-	pair_cut *mixed_pr_cut;
+	pair_cut *real_pair_cut;
+	pair_cut *mixed_pair_cut;
 
 	correlation_collection *correlations;
 	fevent_collection *event_mixing_buffer;
 
+	fevent *preprocess(const event *evt);
+	void fill_particles(track_cut *&cut, track_collection *&src, track_collection *&des);
+	void fill_real_correlation(track_collection *&first, track_collection *&second);
+	void fill_mixed_correlation(track_collection *&first, track_collection *&second);
+
 	bool is_identical_particle() { return (first_track_cut == second_track_cut); }
 	bool is_buffer_full() { return event_mixing_buffer->size() == event_mixing_size; }
-	void fill_particles(track_cut *cut, track_collection *src, particle_collection *des);
 };
+
+inline void analysis::set_event_mixing_size(const unsigned int &size) { event_mixing_size = size; }
+
+inline void analysis::set_event_cut(event_cut *cut) { evt_cut = cut; }
+
+inline void analysis::set_first_track_cut(track_cut *cut) { first_track_cut = cut; }
+
+inline void analysis::set_second_track_cut(track_cut *cut) { second_track_cut = cut; }
+
+inline void analysis::set_real_pair_cut(pair_cut *cut) { real_pair_cut = cut; }
+
+inline void analysis::set_mixed_pair_cut(pair_cut *cut) { mixed_pair_cut = cut; }
+
+inline void analysis::add_correlation(correlation *corr) { correlations->push_back(corr); }
 
 #endif
